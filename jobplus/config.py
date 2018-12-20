c343ce7e31a7bbdbf297fae6b42caef252db2ee7
Liberding <lioding@msn.com>
2018-12-21 00:52:25 +0800

    fix #18 修改config备注，熟悉github

--- jobplus/config.py
+++ jobplus/config.py
@@ -1,14 +1,19 @@
 class BaseConfig:
+    """ 配置基类 """
     SECRET_KEY='codeofteam9-3'
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 
 class DevelopmentConfig(BaseConfig):
+    """ 开发环境配置 """
+    DEBUG = True
     SQLALCHEMY_DATABASE_URI = "mysql://root:root@localhost:3306/jobplus?charset=utf8"
 
 class ProductionConfig(BaseConfig):
+    """ 生产环境配置 """
     pass
 
 class TestingConfig(BaseConfig):
+    """ 测试环境配置 """
     pass
 
 configs = {
