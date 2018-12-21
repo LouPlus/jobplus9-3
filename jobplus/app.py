d9c0e604880f8ec7bc590c884efa83034d659fd3
harvey1114 <44280240+harvey1114@users.noreply.github.com>
2018-12-21 09:39:49 +0800

    fix #16 commit register and login (#17)

--- jobplus/app.py
+++ jobplus/app.py
@@ -1,9 +1,20 @@
 from flask import Flask
 from jobplus.config import configs
-from jobplus.models import db
+from jobplus.models import db, User
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 def register_extentions(app):
     db.init_app(app)
+    Migrate(app,db)
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def user_loader(id):
+        return User.query.get(id)
+
+    login_manager.login_view = 'front.login'
 
 def register_blueprints(app):
     from .handlers import front
