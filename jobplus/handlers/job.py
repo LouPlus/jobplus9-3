5ad01cedefdc03514878044ae492062279017610
python <python@s118.bigdatayh.com>
2018-12-21 17:39:14 +0800

    fix #12 增加翻页条

--- jobplus/handlers/job.py
+++ jobplus/handlers/job.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, current_app
 from jobplus.models import Job
 from datetime import datetime
 
@@ -9,6 +9,12 @@ job = Blueprint('job', __name__, url_prefix='/job')
 
 @job.route('/')
 def index():
-    jobs = Job.query.all()
-#    now = datetime.utcnow()
-    return render_template('job/index.html', jobs=jobs)
+    page = request.args.get('page',default=1, type=int)
+    pagination = Job.query.paginate(
+        page=page,
+        per_page=current_app.config['JOBINDEX_PER_PAGE'],
+        error_out=False
+    )
+
+
+    return render_template('job/index.html', pagination=pagination)
