19a3b67db1f5e4a2e2b3e01154b7e22ea3e8eab6
Esun127 <idol302@163.com>
2018-12-22 13:46:10 +0800

    fix #12 精简salary_range, jtag, jcity表, 并创建一个create_tag页面用来输入这些表的数据, 地址为http://127.0.0.1:5000/job/createtag

--- jobplus/forms.py
+++ jobplus/forms.py
@@ -1,7 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import SelectMultipleField, SelectField, TextAreaField
 from wtforms.validators import DataRequired,Email,Length,EqualTo,ValidationError
-from jobplus.models import db, User
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from jobplus.customfield import TagListField, CityListField
+from jobplus.models import db, User, Job,  get_salary_range
+from jobplus.models import Jtag, Jcity, Salary_Range
+
+import re
 
 class RegisterForm(FlaskForm):
     username = StringField('用户名',validators=[DataRequired(),Length(1,24)])
@@ -29,6 +35,7 @@ class RegisterForm(FlaskForm):
         db.session.commit()
         return user
 
+
 class LoginForm(FlaskForm):
     email = StringField('邮箱', validators=[DataRequired(), Email()])
     password = PasswordField('密码', validators=[DataRequired(), Length(6, 24)])
@@ -43,3 +50,75 @@ class LoginForm(FlaskForm):
         user = User.query.filter_by(email=self.email.data).first()
         if user and not user.check_password(field.data):
             raise ValidationError('密码错误')
+
+
+
+class TagForm(FlaskForm):
+    
+    name = StringField('名称', validators=[DataRequired(), Length(1,20)])
+    submit = SubmitField('提交')
+
+
+
+class AddTagForm(TagForm):
+
+    def validate_name(self, field):
+        if Jtag.query.filter_by(name=field.data).first():
+            raise ValidationError('标签已经存在.')
+
+    def addtag(self):
+        tag = Jtag(name=self.name.data)
+        db.session.add(tag)
+        db.session.commit()
+
+
+
+class AddCityForm(TagForm):
+
+    def validate_name(self, field):
+        if Jcity.query.filter_by(name=field.data).first():
+            raise ValidationError('城市已经存在.')
+
+
+    def addtag(self):
+        tag = Jcity(name=self.name.data)
+        db.session.add(tag)
+        db.session.commit()
+
+
+
+class AddSalaryForm(TagForm):
+
+    def validate_name(self, field):
+        reg = re.compile(r'\d+--\d+')
+        if not reg.fullmatch(field.data):
+            raise ValidationError('请输入<数值>--<数值>')
+
+        if Salary_Range.query.filter_by(name=field.data).first():
+            raise ValidationError('薪资范围已经存在.')
+
+
+    def addtag(self):
+        tag = Salary_Range(name=self.name.data)
+        db.session.add(tag)
+        db.session.commit()
+
+
+class JobForm(FlaskForm):
+    name = StringField('岗位名称', validators=[DataRequired(), Length(1, 32)])
+    requirements = TextAreaField('工作要求', validators=[DataRequired(), Length(1, 1024)])
+    salary_range = QuerySelectField('薪资范围', query_factory=get_salary_range, get_label='srrange')
+    tags = TagListField('关键字')
+    cities = CityListField('工作地点')
+    submit = SubmitField('提交')
+
+
+
+
+
+
+
+
+
+
+    
