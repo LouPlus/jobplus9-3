638d5550897ff6af7e4ac28d8f7769cd0ddf0d6e
Esun127 <idol302@163.com>
2018-12-22 23:58:06 +0800

    fix #12 实现职位创建, 更新, 删除页面

--- jobplus/handlers/job.py
+++ jobplus/handlers/job.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, current_app, flash
 from flask import url_for, redirect
-from jobplus.models import Job
-from jobplus.forms import AddCityForm, AddTagForm, AddSalaryForm
+from jobplus.models import db, Job
+from jobplus.forms import AddCityForm, AddTagForm, AddSalaryForm, JobForm, AddJobForm
 
 
 
@@ -51,23 +51,40 @@ def addtag():
     return render_template('job/addtag.html', tagform=tagform, cityform=cityform, salaryform=salaryform)
 
 
-# @job.route('/createcity', methods=['POST', 'GET'])
-# def addcity():
-#     form = AddCityForm()
-#     if form.validate_on_submit():
-#         form.addtag()
-#         flash('城市添加成功！', 'success')
-#         return url_for('company.addjob')
-#     return render_template('job/addcity.html', form=form)
-#
-#
-# @job.route('/createsalaryrange', methods=['POST', 'GET'])
-# def addsalary():
-#     form = AddSalaryForm()
-#     if form.validate_on_submit():
-#         form.addtag()
-#         flash('薪资范围添加成功！', 'success')
-#         return url_for('company.addjob')
-#     return render_template('job/addsalary.html', form=form)
-#
-#
+
+@job.route('/createjob', methods=['GET', 'POST'])
+def addjob():
+    form = AddJobForm()
+    if form.validate_on_submit():
+        form.addjob()
+        flash('职位添加成功', 'success')
+        return redirect(url_for('.index'))
+
+    return render_template('job/createjob.html', form=form)
+
+
+@job.route('/<int:jobid>/updatejob', methods=['GET', 'POST'])
+def updatejob(jobid):
+    job = Job.query.get_or_404(jobid)
+    form = AddJobForm(obj=job)  # 用表对象填充表单模型对象
+    if form.validate_on_submit():
+        form.updatejob(job)
+        flash('职位更新成功', 'success')
+        return redirect(url_for('.index'))
+    return render_template('job/updatejob.html', form=form, job=job)
+
+
+@job.route('/<int:jobid>/deljob')
+def rmjob(jobid):
+    job = Job.query.get_or_404(jobid)
+    if job:
+        db.session.delete(job)
+        db.session.commit()
+        flash('职位删除成功', 'success')
+    return redirect(url_for('.index'))
+
+
+
+
+
+
